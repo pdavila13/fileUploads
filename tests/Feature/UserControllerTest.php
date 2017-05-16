35d8fb722439259505dc95f9a25802093507e096
Paolo Dávila <pdavila@iesebre.com>
2017-05-16 19:41:27 +0200

    UserControllerTest OK

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Faker\Factory;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUsersAreCreatedOK()
+    {
+        // Prepare
+        $faker = Factory::create();
+
+        $user = [
+            'name'      => $name = $faker->name,
+            'email'     => $email = $faker->unique()->safeEmail,
+            'password'  => $password = bcrypt('secret')
+        ];
+
+        // Login a authorized user and Execute
+        $authorizeUser = factory(User::class)->create();
+        $response = $this->actingAs($authorizeUser, 'api')->json('POST', 'api/v1/user', $user);
+
+        // Assert
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'created' => true,
+            ]);
+
+        $this->assertDatabaseHas('users', [
+            'name'      => $name,
+            'email'     => $email,
+            'password'  => $password
+        ]);
+    }
+}
